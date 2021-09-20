355fac11ea15379d0da9799469c14c0cea3a7426
Kereeshkaa <kiril.suff1@gmail.com>
2021-09-20 21:19:09 +0300

    DZ 2

--- lesson1.py
+++ lesson1.py
@@ -1,7 +1,60 @@
-print("Hello, World")
-# comment
-print("Hello, World")
+my_value = 7
+my_bool_1 = my_value % 3 == 0
+my_bool_2 = my_value % 2 == 0
+print(not my_bool_1 and not my_bool_2)
 
-print(123 * 2, "456", "qwe")
-print(15 - 4)
-print(123 * 5 + 84 / 4)
+# my_value = 14
+# my_bool_1 = my_value % 3 == 0
+# my_bool_2 = my_value % 2 == 0
+# print(my_bool_1 or my_bool_2)
+
+# my_value = 12
+# my_bool_1 = my_value % 3 == 0
+# my_bool_2 = my_value % 2 == 0
+# print(my_bool_1 and my_bool_2)
+#
+
+
+# my_value = 12
+# my_bool = my_value % 3 == 0
+# print(my_bool)
+
+
+# my_value_1 = 2
+# my_value_2 = '4'
+#
+# my_value_1 = str(my_value_1)
+#
+# print(my_value_1 + my_value_2)
+
+
+# my_value_1 = 2
+# my_value_2 = '4'
+#
+# my_value_2 = int(my_value_2)
+#
+# print(my_value_1 * my_value_2)
+#
+
+
+# my_value_1 = 2
+# my_value_2 = '4'
+#
+# my_value_1 = str(my_value_1)
+# my_value_2 = int(my_value_2)
+#
+# print(my_value_1 * my_value_2)
+
+
+# my_value_1 = 2
+# my_value_2 = '4'
+# print(my_value_1 * my_value_2)
+
+
+# a = 'Hello, "A"'
+# b = a
+# c = 'Hello, "B"'
+# b = c
+# c = a
+# a = b
+# print(a)
