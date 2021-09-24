d06c558c18987ae1939879f8903804fff85354e8
Kereeshkaa <kiril.suff1@gmail.com>
2021-09-24 15:37:27 +0300

    silence code

--- lesson1.py
+++ lesson1.py
@@ -1,33 +1,29 @@
-my_value = 7
-my_bool_1 = my_value % 3 == 0
-my_bool_2 = my_value % 2 == 0
-print(not my_bool_1 and not my_bool_2)
-
+# my_value = 7
+# my_bool_1 = my_value % 3 == 0
+# my_bool_2 = my_value % 2 == 0
+# print(not my_bool_1 and not my_bool_2)
+#
 # my_value = 14
 # my_bool_1 = my_value % 3 == 0
 # my_bool_2 = my_value % 2 == 0
 # print(my_bool_1 or my_bool_2)
-
+#
 # my_value = 12
 # my_bool_1 = my_value % 3 == 0
 # my_bool_2 = my_value % 2 == 0
 # print(my_bool_1 and my_bool_2)
 #
-
-
 # my_value = 12
 # my_bool = my_value % 3 == 0
 # print(my_bool)
-
-
+#
 # my_value_1 = 2
 # my_value_2 = '4'
 #
 # my_value_1 = str(my_value_1)
 #
 # print(my_value_1 + my_value_2)
-
-
+#
 # my_value_1 = 2
 # my_value_2 = '4'
 #
@@ -35,8 +31,6 @@ print(not my_bool_1 and not my_bool_2)
 #
 # print(my_value_1 * my_value_2)
 #
-
-
 # my_value_1 = 2
 # my_value_2 = '4'
 #
@@ -44,13 +38,11 @@ print(not my_bool_1 and not my_bool_2)
 # my_value_2 = int(my_value_2)
 #
 # print(my_value_1 * my_value_2)
-
-
+#
 # my_value_1 = 2
 # my_value_2 = '4'
 # print(my_value_1 * my_value_2)
-
-
+#
 # a = 'Hello, "A"'
 # b = a
 # c = 'Hello, "B"'
